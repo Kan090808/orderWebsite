9e5bdbf33cefc7849297e41e5342d79f64745ca4
root <root@instance-2.c.uplifted-light-250806.internal>
2019-09-04 09:30:22 +0000

    config

---
+++ config-sample.php
@@ -0,0 +1,12 @@
+<?php
+global $db;
+$db["server"] = "localhost";
+$db["user"] = "";
+$db["password"] = "";
+$db["dbname"] = "order";
+// Create connection
+$conn = new mysqli($db["server"], $db["user"], $db["password"], $db["dbname"]);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully<br/>";
